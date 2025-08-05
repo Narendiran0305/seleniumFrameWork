e79bc3fc5d8e21b22b0eb3846db19473156ead42
Narendiran <narendiran2001@gmail.com>
2025-08-05 06:51:47 +0530

    TestCase6

---
+++ src/test/java/TestCases/TestCase6.java
@@ -0,0 +1,73 @@
+package TestCases;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+@Listeners(ListenerClass.class)
+public class TestCase6 {
+
+	WebDriver driver;
+
+	@BeforeTest
+	public void launchbrower() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--incognito");
+		driver = new ChromeDriver(options);
+
+		driver.get("https://practice-automation.com/broken-links/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+	}
+
+	@AfterTest
+	public void closebrowser() {
+		driver.quit();
+	}
+
+	@Test
+	public void windowHandleTest() throws MalformedURLException, IOException {
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		for(WebElement link : links)
+		{
+			String url = link.getAttribute("href");
+			
+			if(!url.isEmpty())
+			{
+					HttpURLConnection connection = (HttpURLConnection)(new URL(url).openConnection());
+					connection.connect();
+					int responseCode = connection.getResponseCode();
+					if(responseCode >= 400)
+					{
+						System.out.println(url+" -> is broken");
+					}
+					else
+					{
+						System.out.println(url+" -> is working");
+					}
+			}
+			
+		}
+	}
+
+}
